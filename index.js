8c230551afdd21ff83657ae82e8f153c2b552784
Alexnander Nasokin <alexnas2python@gmail.com>
2022-08-30 11:28:07 +0300

    Use cors and json parsing

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const sequelize = require('./src/config/db.config')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 const start = async () => {
 	try {
