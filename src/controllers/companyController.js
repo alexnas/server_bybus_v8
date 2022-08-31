fa62ddc011a4415388a639ed48bdb3384466e209
Alexnander Nasokin <alexnas2python@gmail.com>
2022-08-31 19:23:54 +0300

    Make CRUD for Company entity

--- src/controllers/companyController.js
+++ src/controllers/companyController.js
@@ -1,22 +1,79 @@
-class CompanyController {
-	async create(req, res) {
+const ApiError = require('../errors/ApiError')
+const { Company } = require('../models/models')
 
+class CompanyController {
+	async create(req, res, next) {
+		const {name, fullname, logo, rating, description} = req.body
+		try {
+			if (!name || name.trim() === '') {
+				return next(ApiError.wrongValue('Name is not defined'))
+			}
+			const candidate = await Company.findOne({where: {name}})
+			if (candidate) {
+				return next(ApiError.wrongValue('This company name is already registered'))
+			}
+						
+			const company = await Company.create({name, fullname, logo, rating, description})
+			res.json(company)
+		} catch (e) {
+			return next(ApiError.internal('Server error'))
+		} 
 	}
 
 	async getAll(req, res) {
-
+		const companies = await Company.findAll()
+		res.json(companies)
 	}
 
-	async getOne(req, res) {
-
+	async getOne(req, res, next) {
+		const {id} = req.params
+		try {
+			const company = await Company.findOne({where: {id}})
+			if (!company) {
+				return next(ApiError.badRequest('This company is not registered'))
+			}
+			res.json(company)
+		} catch (e) {
+			return next(ApiError.internal('Server error'))
+		}
 	}
 
-	async update(req, res) {
-		
+	async update(req, res, next) {
+		const {id} = req.params
+		let {name, fullname, logo, rating, description} = req.body
+		try {
+			if (!name || name.trim() === '') {
+				return next(ApiError.wrongValue('Name is not defined'))
+			}
+			const candidate = await Company.findOne({where: {name}})
+			if (candidate && (candidate.id !== +id)) {
+				return next(ApiError.wrongValue('This company name is already registered'))
+			}
+
+			let company = await Company.findOne({where: {id}})
+			if (!company) {
+					return next(ApiError.badRequest('There is no such company registered'))
+			}
+
+			await company.update({name, fullname, logo, rating, description})
+			res.json(company)
+		} catch (e) {
+			return next(ApiError.internal('Server error'))
+		}
 	}
 
-	async delete(req, res) {
-		
+	async delete(req, res, next) {
+		const {id} = req.params
+		try {
+			const company = await Company.findOne({where: {id}})
+			if (!company) {
+				return next(ApiError.badRequest('This company is not registered'))
+			}
+			company.destroy()
+			res.json(company)
+		} catch (e) {
+			return next(ApiError.internal('Server error'))
+		}
 	}
 }
 
